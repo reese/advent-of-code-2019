0ff75aece55e9a50200bcc1fe066f0d541d4bb58
Reese Williams <rtwill722@gmail.com>
2019-12-01 11:38:00 -0500

    Complete Day 1 Part 2.

--- src/day1.rs
+++ src/day1.rs
@@ -8,7 +8,28 @@ pub fn day1_generator(input: &str) -> Vec<i32> {
 
 #[aoc(day1, part1)]
 pub fn part1(module_mass: &Vec<i32>) -> i32 {
-  module_mass.iter().map(|mass| (mass / 3) - 2).sum()
+  module_mass.iter().map(|mass| fuel_for_mass(mass)).sum()
+}
+
+#[aoc(day1, part2)]
+pub fn part2(module_mass: &Vec<i32>) -> i32 {
+  module_mass
+    .iter()
+    .map(|mass| fuel_for_mass_recursive(mass))
+    .sum()
+}
+
+fn fuel_for_mass(mass: &i32) -> i32 {
+  (mass / 3) - 2
+}
+
+fn fuel_for_mass_recursive(mass: &i32) -> i32 {
+  let required_fuel = fuel_for_mass(mass);
+  if required_fuel > 0 {
+    required_fuel + fuel_for_mass_recursive(&required_fuel)
+  } else {
+    0
+  }
 }
 
 #[cfg(test)]
@@ -16,22 +37,17 @@ mod tests {
   use super::*;
 
   #[test]
-  fn it_works_for_base_case() {
-    assert_eq!(part1(&vec!(12)), 2)
+  fn part_one_works_for_example_cases() {
+    assert_eq!(part1(&vec!(12)), 2);
+    assert_eq!(part1(&vec!(14)), 2);
+    assert_eq!(part1(&vec!(1969)), 654);
+    assert_eq!(part1(&vec!(100756)), 33583);
   }
 
   #[test]
-  fn it_rounds_down() {
-    assert_eq!(part1(&vec!(14)), 2)
-  }
-
-  #[test]
-  fn it_works_for_larger_input() {
-    assert_eq!(part1(&vec!(1969)), 654)
-  }
-
-  #[test]
-  fn it_works_for_even_larget_input() {
-    assert_eq!(part1(&vec!(100756)), 33583)
+  fn part_two_works_for_example_cases() {
+    assert_eq!(part2(&vec!(14)), 2);
+    assert_eq!(part2(&vec!(1969)), 966);
+    assert_eq!(part2(&vec!(100756)), 50346);
   }
 }
