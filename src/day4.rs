74a40ece91fc1306c1f3e004ddf7819a86b6f14e
Reese Williams <rtwill722@gmail.com>
2019-12-22 16:43:24 -0500

    Add Day 4 Part 2.

--- src/day4.rs
+++ src/day4.rs
@@ -1,6 +1,7 @@
 extern crate itertools;
 
 use itertools::Itertools;
+use std::collections::HashMap;
 
 #[aoc_generator(day4)]
 fn day_four_generator(input: &str) -> Vec<i64> {
@@ -8,7 +9,7 @@ fn day_four_generator(input: &str) -> Vec<i64> {
 }
 
 #[aoc(day4, part1)]
-fn day_four(input: &Vec<i64>) -> i64 {
+fn day_four_part_one(input: &Vec<i64>) -> i64 {
     let mut count = 0;
     for number in *input.get(0).unwrap()..*input.get(1).unwrap() {
         let number_as_string = number.to_string();
@@ -18,11 +19,26 @@ fn day_four(input: &Vec<i64>) -> i64 {
             all_digits_are_ascending(&number_as_string) &&
             has_double_digit(&number_as_string){
             count += 1;
-            println!("{:?}: (current_count: {:?})", number_as_string, count);
         }
     }
     count
 }
+#[aoc(day4, part2)]
+fn day_four_part_two(input: &Vec<i64>) -> i64 {
+    let mut count = 0;
+    for number in *input.get(0).unwrap()..*input.get(1).unwrap() {
+        let number_as_string = number.to_string();
+
+
+        if has_six_digits(&number_as_string) && // This _should_ be true given the input, but checking for portability
+            all_digits_are_ascending(&number_as_string) &&
+            has_exact_double_digit(&number_as_string){
+            count += 1;
+        }
+    }
+    count
+}
+
 
 fn has_six_digits(input: &String) -> bool {
     input.len() == 6
@@ -38,6 +54,15 @@ fn has_double_digit(input: &String) -> bool {
     digits.into_iter().tuple_windows().any(|(prev, next)| prev == next)
 }
 
-#[cfg(test)]
-mod test {
+fn has_exact_double_digit(input: &String) -> bool {
+    let digits = input.chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<_>>();
+    let mut double_digit_count: HashMap<u32, u32> = HashMap::new();
+    digits.into_iter().tuple_windows().for_each(|(prev, next)| {
+        if prev == next {
+            let counter = double_digit_count.entry(prev).or_insert(0);
+            *counter += 1;
+        }
+    });
+    double_digit_count.values().any(|value| *value == 1)
 }
+
